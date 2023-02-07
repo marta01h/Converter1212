7a843177b7fd0f45a86f8006381928ddde5d0162
marta01h <isip_m.a.sargsyan@mpt.ru>
2023-02-08 00:16:49 +0300

    Converter

---
+++ Converter/Program.cs
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm> 
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    std::string line, fPth;
+    cout << "--Форматирование шейдеров--\n\n";
+
+    if (argc <= 1)
+    {
+        cout << "--Выполняется без файлов--\n";
+        system("Пауза");
+        return 0;
+    }
+    else
+    {
+        for (int i(1); i < argc; ++i)
+        {
+            fPth = argv[i];
+            ifstream inFile(fPth);
+            if (inFile.is_open())
+            {
+                ofstream outFile("В линии" + to_string(i) + ".txt");
+        if (outFile)
+        {
+            while (getline(inFile, line))
+            {
+                if (!line.empty())
+                {
+                    line.erase(std::remove_if(line.begin(), line.end(), [](char c) { return (c == '\r' || c == '\t' || c == ' ' || c == '\n'); }), line.end());
+                    if (line != "")
+                        outFile << "\"" << line << "\"\n";
+                }
+            }
+            cout << "--Всё гтово! Проверьте \"в линии--" + to_string(i) + ".txt\".--\n";
+        }
+        else cout << "--Не удается записать в файл--\n";
+        outFile.close();
+    }
+
+            else cout << "--Не удается открыть файл--\n";
+    inFile.close();
+}
+	}
+	system("Пауза");
+return 0;
+}
